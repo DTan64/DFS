f05f220e7627df682e17c9311b978fcb53d91b59
D'Artagnan Wake <dwake70@gmail.com>
2018-12-05 13:13:26 -0700

    readConf function complete

--- dfclient.c
+++ dfclient.c
@@ -15,12 +15,21 @@
 
 #define MAXBUFSIZE 2048
 
+typedef struct ServerConf {
+  char* ip;
+  char* port;
+  int number;
+} ServerConf;
+
+void readConf(char* confName, ServerConf* servers, char** username, char** password);
+
 int main (int argc, char * argv[])
 {
 
-	int nbytes;                             // number of bytes send by sendto()
+	int nbytes, readBytes;                             // number of bytes send by sendto()
 	int sock;                               //this will be our socket
 	char buffer[MAXBUFSIZE];
+	char fileBuffer[MAXBUFSIZE];
 	bool flag = false;
 	char userInput[MAXBUFSIZE];
 	char fileName[MAXBUFSIZE];
@@ -33,19 +42,33 @@ int main (int argc, char * argv[])
 	char exitServer[] = "exit";
 	char over[] = "Over";
 	int fd;
+  FILE* confFD;
 	char* splitInput;
+  char* saveptr;
+  char* username;
+  char* password;
+  char* ip;
+  ServerConf servers[4];
 
-	if (argc < 3)
-	{
-		printf("USAGE:  <server_ip> <server_port>\n");
+	if (argc < 2) {
+		printf("USAGE: <dfc.conf> \n");
 		exit(1);
 	}
 
-	/******************
-	  Here we populate a sockaddr_in struct with
-	  information regarding where we'd like to send our packet
-	  i.e the Server.
-	 ******************/
+  readConf(argv[1], servers, &username, &password);
+
+  int i;
+  printf("Username: %s\n", username);
+  printf("Password: %s\n", password);
+  for(i = 0; i < 4; i++) {
+    printf("number: %i\n", servers[i].number);
+    printf("ip: %s\n", servers[i].ip);
+    printf("port: %s\n", servers[i].port);
+  }
+
+
+
+
 	bzero(&remote_addr,sizeof(remote_addr));               //zero the struct
 	remote_addr.sin_family = AF_INET;                 //address family
 	remote_addr.sin_port = htons(10001);      //sets port to network byte order
@@ -83,4 +106,53 @@ int main (int argc, char * argv[])
 
 	}
 	close(sock);
+}
+
+void readConf(char* confName, ServerConf* servers, char** username, char** password)
+{
+  // Read conf file
+  FILE* confFD;
+  char* saveptr;
+	char* splitInput;
+  char* ip;
+  char fileBuffer[MAXBUFSIZE];
+  confFD = fopen(confName, "r");
+  if(confFD == NULL) {
+    printf("Error opening file.\n");
+    exit(0);
+  }
+  bzero(fileBuffer,sizeof(fileBuffer));
+  int serverNum;
+  while(1) {
+    if(fgets(fileBuffer, sizeof(fileBuffer), confFD)!=NULL ) {
+      if(!strncmp(&fileBuffer[0], "U", 1)) {
+        splitInput = strtok_r(fileBuffer, " ", &saveptr);
+        splitInput = strtok_r(NULL, " ", &saveptr);
+        *username = (char*) malloc(strlen(splitInput));
+        strcpy(*username, splitInput);
+      }
+      else if(!strncmp(&fileBuffer[0], "P", 1)) {
+        splitInput = strtok_r(fileBuffer, " ", &saveptr);
+        splitInput = strtok_r(NULL, " ", &saveptr);
+        *password = (char*) malloc(strlen(splitInput));
+        strcpy(*password, splitInput);
+      }
+      else {
+        splitInput = strtok_r(fileBuffer, " ", &saveptr);
+        serverNum = atoi(&splitInput[strlen(splitInput) - 1]);
+        servers[serverNum - 1].number = serverNum;
+        splitInput = strtok_r(NULL, " ", &saveptr);
+        ip = strtok_r(splitInput, ":", &saveptr);
+        servers[serverNum - 1].ip = (char *) malloc(strlen(ip));
+        strcpy(servers[serverNum - 1].ip, ip);
+        ip = strtok_r(NULL, ":", &saveptr);
+        servers[serverNum - 1].port = (char *) malloc(strlen(ip));
+        strcpy(servers[serverNum - 1].port, ip);
+      }
+    }
+    else {
+      break;
+    }
+  }
+
 }
